0cc73ee35060cee4920cb632e77ebf94093ff900
Sidney Andrews <sidney@seesharprun.net>
2015-11-19 09:34:21 -0500

    Project Files

---
+++ RestaurantManager.ViewModels/ViewModel.cs
@@ -0,0 +1,46 @@
+﻿using RestaurantManager.Models;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace RestaurantManager.ViewModels
+{
+    public abstract class ViewModel : INotifyPropertyChanged
+    {
+        private bool _isLoading;
+        protected RestaurantContext Repository { get; private set; }
+
+        public ViewModel()
+        {
+            LoadData();
+        }
+
+        private async void LoadData()
+        {
+            this.Repository = await RestaurantContextFactory.GetRestaurantContextAsync();
+            OnDataLoaded();
+        }
+
+        protected abstract void OnDataLoaded();
+        
+        public bool IsLoading
+        {
+            get { return _isLoading; }
+            set
+            {
+                _isLoading = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
